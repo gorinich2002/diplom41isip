35e3d8f91ef39629c7e0e1ae5cd12939a03e554e
gorinich2002 <egormaysnikov@gmail.com>
2022-06-13 23:38:27 +0300

    delete midleware

--- routes/order.routers.js
+++ routes/order.routers.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const Order = require("../models/Order");
-const auth = require("../middleware/auth.middleware")
 const mailSend = require('../mailer')
 const router = Router();
 router.post(
@@ -13,19 +12,6 @@ router.post(
       await order.save();
       mailSend(clientData.mail, req.body)
       res.status(201).json({ message: "Заказ добавлен" });
-    } catch (e) {
-      res.status(500).json({ message: "Error, reload your page" });
-      console.log(e);
-    }
-  }
-);
-router.post(
-  "/orders",
-  auth,
-  async (req, res) => {
-    try {
-      const orders = await Order.find({isSended: false});
-      res.json(orders);
     } catch (e) {
       res.status(500).json({ message: "Error, reload your page" });
       console.log(e);
